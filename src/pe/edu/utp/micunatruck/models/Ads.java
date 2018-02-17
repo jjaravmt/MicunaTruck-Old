6be8aa171d9e27b271db39ea9fa12fee5fa4c951
jose <jjaravmt@gmail.com>
2018-02-17 03:01:11 +0100

    Mod. Clase Ads, se agrego campo codesp (cdigo espacio)

--- src/pe/edu/utp/micunatruck/models/Ads.java
+++ src/pe/edu/utp/micunatruck/models/Ads.java
@@ -14,17 +14,17 @@ import java.util.Date;
         private Date deletedAt = new Date();
         private Date updatedAt = new Date();
         private Date createdAt = new Date();
+        /****Campo pendiente de agregar a la bd jose luis  16/02/2018 ****/
+        private int codesp;
 
 
         public Ads() {
 
         }
 
-        public Ads(int id, int admin_id, String name, String description,
-                   String image, double price, Date start_date, Date end_date,
-                   boolean flagActive, Date deletedAt, Date updatedAt, Date createdAt) {
+        public Ads(int id, Admin admin, String name, String description, String image, double price, Date start_date, Date end_date, boolean flagActive, Date createdAt) {
             this.setId(id);
-            /*this.setAdmin_id(admin_id);*/
+            this.setAdmin(admin);
             this.setName(name);
             this.setDescription(description);
             this.setImage(image);
@@ -32,11 +32,8 @@ import java.util.Date;
             this.setStart_date(start_date);
             this.setEnd_date(end_date);
             this.setFlagActive(flagActive);
-            this.setDeletedAt(deletedAt);
-            this.setUpdatedAt(updatedAt);
             this.setCreatedAt(createdAt);
         }
-
 
         public int getId() {
             return id;
@@ -47,14 +44,14 @@ import java.util.Date;
             return this;
         }
 
-        /*public int getAdmin_id() {
-            return admin_id;
-        }*/
+        public Admin getAdmin() {
+            return admin;
+        }
 
-        /*public Ads setAdmin_id(int admin_id) {
-            this.admin_id = admin_id;
+        public Ads setAdmin(Admin admin) {
+            this.admin = admin;
             return this;
-        }*/
+        }
 
         public String getName() {
             return name;
@@ -145,6 +142,142 @@ import java.util.Date;
             this.createdAt = createdAt;
             return this;
         }
+
+        public int getCodesp() {
+            return codesp;
+        }
+
+        public Ads setCodesp(int codesp) {
+            this.codesp = codesp;
+            return this;
+        }
+
+        /*public Ads(int id, int admin_id, String name, String description,
+                   String image, double price, Date start_date, Date end_date,
+                   boolean flagActive, Date deletedAt, Date updatedAt, Date createdAt) {
+            this.setId(id);
+            this.setAdmin_id(admin_id);
+            this.setName(name);
+            this.setDescription(description);
+            this.setImage(image);
+            this.setPrice(price);
+            this.setStart_date(start_date);
+            this.setEnd_date(end_date);
+            this.setFlagActive(flagActive);
+            this.setDeletedAt(deletedAt);
+            this.setUpdatedAt(updatedAt);
+            this.setCreatedAt(createdAt);
+        }*/
+
+
+
+        /*public int getId() {
+            return id;
+        }*/
+
+        /*public Ads setId(int id) {
+            this.id = id;
+            return this;
+        }*/
+
+        /*public int getAdmin_id() {
+            return admin_id;
+        }*/
+
+        /*public Ads setAdmin_id(int admin_id) {
+            this.admin_id = admin_id;
+            return this;
+        }*/
+
+        /*public String getName() {
+            return name;
+        }*/
+
+        /*public Ads setName(String name) {
+            this.name = name;
+            return this;
+        }*/
+
+        /*public String getDescription() {
+            return description;
+        }*/
+
+        /*public Ads setDescription(String description) {
+            this.description = description;
+            return this;
+        }*/
+
+        /*public String getImage() {
+            return image;
+        }*/
+
+        /*public Ads setImage(String image) {
+            this.image = image;
+            return this;
+        }*/
+
+        /*public double getPrice() {
+            return price;
+        }*/
+
+        /*public Ads setPrice(double price) {
+            this.price = price;
+            return this;
+        }*/
+
+        /*public Date getStart_date() {
+            return start_date;
+        }*/
+
+        /*public Ads setStart_date(Date start_date) {
+            this.start_date = start_date;
+            return this;
+        }*/
+
+        /*public Date getEnd_date() {
+            return end_date;
+        }*/
+
+        /*public Ads setEnd_date(Date end_date) {
+            this.end_date = end_date;
+            return this;
+        }*/
+
+        /*public boolean isFlagActive() {
+            return flagActive;
+        }*/
+
+        /*public Ads setFlagActive(boolean flagActive) {
+            this.flagActive = flagActive;
+            return this;
+        }*/
+
+        /*public Date getDeletedAt() {
+            return deletedAt;
+        }*/
+
+        /*public Ads setDeletedAt(Date deletedAt) {
+            this.deletedAt = deletedAt;
+            return this;
+        }*/
+
+        /*public Date getUpdatedAt() {
+            return updatedAt;
+        }*/
+
+        /*public Ads setUpdatedAt(Date updatedAt) {
+            this.updatedAt = updatedAt;
+            return this;
+        }*/
+
+        /*public Date getCreatedAt() {
+            return createdAt;
+        }*/
+
+        /*public Ads setCreatedAt(Date createdAt) {
+            this.createdAt = createdAt;
+            return this;
+        }*/
     }
 
 
